373751945ebb7b0d2dbd17d8b5601e07f060e7db
margarethaywood <openemr@cmsvt.com>
2026-02-03 15:15:34 -0600

    accepted merge update

--- interface/forms/eye_mag/view.php
+++ interface/forms/eye_mag/view.php
@@ -4376,24 +4376,12 @@ if ($refresh and $refresh != 'fullscreen') {
             <?php endif; ?>
         }
         function doscript(type,id,encounter,rx_number) {
-<<<<<<< HEAD
-              // Get the checked radio button's value
-              let rxType = document.querySelector('input[name="RX_TYPE_1"]:checked')?.value;
-             const params = new URLSearchParams({
-                 REFTYPE: type,
-                 id: id,
-                 encounter: encounter,
-                 form_id: <?php echo js_escape($form_id); ?>,
-                 rx_number: rx_number,
-                 rx_type: rxType
-=======
              const params = new URLSearchParams({
                  encounter: encounter,
                  form_id: <?php echo js_escape($form_id); ?>,
                  id: id,
                  REFTYPE: type,
                  rx_number: rx_number
->>>>>>> master
              });
              dlgopen('../../forms/eye_mag/SpectacleRx.php?' + params.toString(), '_blank', 660, 700,'', <?php echo xlj('Dispense Rx'); ?>);
         }
